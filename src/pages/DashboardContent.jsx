262295e3ddda0b42892ef9ef202b739ceed5438b
CSMathematics <spyrosfronimos@gmail.com>
2025-07-21 12:19:05 +0300

    Firebase loading issue fixed for the week scheddule

--- src/pages/DashboardContent.jsx
+++ src/pages/DashboardContent.jsx
@@ -6,37 +6,42 @@ import { Container } from '@mui/material';
 import DashboardStats from './DashboardStats.jsx';
 import QuickActions from './QuickActions.jsx';
 import LicenseUsage from './LicenseUsage.jsx';
-import WeeklyScheduleCalendar from './WeeklyScheduleCalendar.jsx'; // Import your existing WeeklyScheduleCalendar
+import WeeklyScheduleCalendar from './WeeklyScheduleCalendar.jsx'; // Import the actual calendar
 import YearFilter from './YearFilter.jsx';
 import ChartPlaceholder from './ChartPlaceholder.jsx';
 
-function DashboardContent({ onNewStudentClick, onStudentsListClick, onNewClassroomClick, onClassroomsListClick, navigateTo, classrooms, loadingClassrooms, db, userId }) {
+function DashboardContent({
+    onNewStudentClick,
+    onStudentsListClick,
+    onNewClassroomClick, // Added
+    onClassroomsListClick, // Added
+    navigateTo, // Added
+    classrooms, // Added
+    loadingClassrooms, // Changed from 'loading' to 'loadingClassrooms' to match App.jsx
+    onCreateClassroomFromCalendar, // Added
+    onEditClassroom, // Added
+    db, userId, appId // Added Firebase props
+}) {
     return (
         <Container maxWidth="lg">
             <DashboardStats />
             <QuickActions
                 onNewStudentClick={onNewStudentClick}
                 onStudentsListClick={onStudentsListClick}
-                onNewClassroomClick={onNewClassroomClick}
-                onClassroomsListClick={onClassroomsListClick}
+                onNewClassroomClick={onNewClassroomClick} // Pass down
+                onClassroomsListClick={onClassroomsListClick} // Pass down
             />
             <LicenseUsage />
-            {/* Render WeeklyScheduleCalendar instead of CalendarPlaceholder */}
+            {/* Render the actual WeeklyScheduleCalendar here */}
             <WeeklyScheduleCalendar
                 classrooms={classrooms}
-                loading={loadingClassrooms}
-                onCreateClassroomFromCalendar={(initialSchedule) => {
-                    // This callback will be triggered by WeeklyScheduleCalendar
-                    // We need to pass it up to App.jsx to navigate to NewClassroomForm
-                    navigateTo('newClassroom', { initialSchedule }); // Pass initialSchedule as part of navigation
-                }}
-                onEditClassroom={(classroom) => {
-                    // This callback will be triggered by WeeklyScheduleCalendar when an event is clicked
-                    navigateTo('newClassroom', { classroomToEdit: classroom });
-                }}
-                navigateTo={navigateTo} // Pass navigateTo for internal calendar navigation if needed
-                db={db} // Pass db instance
-                userId={userId} // Pass userId
+                loading={loadingClassrooms} // Pass loadingClassrooms to calendar
+                onCreateClassroomFromCalendar={onCreateClassroomFromCalendar}
+                onEditClassroom={onEditClassroom}
+                navigateTo={navigateTo}
+                db={db}
+                userId={userId}
+                appId={appId}
             />
             <YearFilter />
             <ChartPlaceholder title="Attendance by Subject" icon="fas fa-chart-bar" chartId="attendanceChart" />
